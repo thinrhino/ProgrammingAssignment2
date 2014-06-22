1e87cc706a387faf8bcac59b4ffab6319bb3ea79
Aditya Laghate <me@adityalaghate.in>
2014-06-22 18:07:16 +0530

    Added sample run to code

--- cachematrix.R
+++ cachematrix.R
@@ -40,3 +40,25 @@ cacheSolve <- function(x, ...) {
     x$setinv(inv)
     inv
 }
+
+## Sample run:
+## > x = rbind(c(1, -22/7), c(-22/7, 1))
+## > m = makeCacheMatrix(x)
+## > m$get()
+## [,1] [,2]
+## [1,] 1.00 -0.25
+## [2,] -0.25 1.00
+
+## No cache in the first run
+## > cacheSolve(m)
+## [,1] [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Retrieving from the cache in the second run
+## > cacheSolve(m)
+## getting cached data.
+## [,1] [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## > 
